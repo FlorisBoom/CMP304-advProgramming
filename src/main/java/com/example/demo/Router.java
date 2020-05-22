619563ead022331574cabd155f92a50128497ea7
Floris Boom <florisboom1@gmail.com>
2020-05-22 03:32:04 +0200

    almost done

--- src/main/java/com/example/demo/Router.java
+++ src/main/java/com/example/demo/Router.java
@@ -6,12 +6,25 @@ import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.gson.Gson;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,10 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -35,7 +45,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class Router {
 
     Firestore db;
+    FirebaseAuth auth;
     String id = "";
+    String refreshToken = "";
 
     public Router() throws IOException {
         InputStream serviceAccount = new FileInputStream("cmp304-advprogramming-firebase-adminsdk-eaken-75845526b6.json");
@@ -64,6 +76,16 @@ public class Router {
         DocumentReference docRef = db.collection("flights").document(id);
         ApiFuture<DocumentSnapshot> flight = docRef.get();
         DocumentSnapshot document = flight.get();
+        results.put(document.getId(), document.getData());
+        return results;
+    }
+
+    @RequestMapping(value = "/booking/{id}", method = GET)
+    public JSONObject booking(@PathVariable("id") String id) throws Exception {
+        JSONObject results = new JSONObject();
+        DocumentReference docRef = db.collection("bookings").document(id);
+        ApiFuture<DocumentSnapshot> booking = docRef.get();
+        DocumentSnapshot document = booking.get();
         results.put(document.getId(), document.getData());
         return results;
     }
@@ -128,6 +150,80 @@ public class Router {
         }
 
         return flightDetails;
+    }
+
+    @RequestMapping(value = "/allFlights", method = GET)
+    public JSONObject allFlights() throws Exception {
+        JSONObject allFlights = new JSONObject();
+        ApiFuture<QuerySnapshot> query = db.collection("flights").get();
+        List<QueryDocumentSnapshot> documents = query.get().getDocuments();
+        for (QueryDocumentSnapshot document: documents) {
+            allFlights.put(document.getId(), document.getData());
+        }
+        return allFlights;
+    }
+
+    @RequestMapping(value = "/allBookings", method = GET)
+    public JSONObject allBookings() throws Exception {
+        JSONObject allBookings = new JSONObject();
+        ApiFuture<QuerySnapshot> query = db.collection("bookings").get();
+        List<QueryDocumentSnapshot> documents = query.get().getDocuments();
+        for (QueryDocumentSnapshot document: documents) {
+            allBookings.put(document.getId(), document.getData());
+        }
+        return allBookings;
+    }
+
+    @RequestMapping(value = "/login", method = POST)
+    public JSONObject login(@RequestParam("email") String email, @RequestParam("password") String password ) throws FirebaseAuthException, IOException {
+        Dotenv dotenv = Dotenv.load();
+
+        String url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + dotenv.get("API_KEY");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(url);
+
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        params.add(new BasicHeader("returnSecureToken", "true"));
+        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity entity = response.getEntity();
+        String result = EntityUtils.toString(entity);
+        JSONObject convertedObject = new Gson().fromJson(result, JSONObject.class);
+        refreshToken = (String) convertedObject.get("refreshToken");
+
+        return convertedObject;
+    }
+
+    @RequestMapping(value = "/getToken", method = GET)
+    public String token() {
+        return refreshToken;
+    }
+
+
+    @RequestMapping(value = "/deleteFlight", method = POST)
+    public String deleteFlight(@RequestParam("id") String id) {
+        boolean isDeleted = false;
+
+        ApiFuture<WriteResult> writeResult = db.collection("flights").document(id).delete();
+
+        isDeleted = true;
+
+        return id;
+    }
+
+    @RequestMapping(value = "/deleteBooking", method = POST)
+    public String deleteBooking(@RequestParam("id") String id) {
+        boolean isDeleted = false;
+
+        ApiFuture<WriteResult> writeResult = db.collection("bookings").document(id).delete();
+
+        isDeleted = true;
+
+        return id;
     }
 
     @RequestMapping(value = "/test", method = GET)
